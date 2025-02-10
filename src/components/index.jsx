482b9f985cdf34a4f759863a16494a5752f70086
Galin7777 <kazakova0309@yandex.ru>
2025-02-10 11:12:29 +0300

    Added Dialogs

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,4 @@
 export { Header } from './Header';
 export { Navbar } from './Navbar';
 export { Profile } from './Profile';
-export { ProfileInfo } from './Profile/ProfileInfo/';
-export { Post } from './Post';
 export { Dialogs } from './Dialogs';
