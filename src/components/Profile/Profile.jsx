b8126dc699c523b0fb851a4b818a50739eec48d2
Galin7777 <kazakova0309@yandex.ru>
2025-02-23 16:05:52 +0300

    Added dispatch

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,14 @@
-// import classes from './Profile.module.scss';
 import React from 'react';
 import { MyPosts } from './MyPosts/MyPosts';
 import { ProfileInfo } from './ProfileInfo/ProfileInfo';
-import { addPost } from '../../redux/state';
 
 export const Profile = (props) => {
   return (
     <div>
       <ProfileInfo />
-      <MyPosts posts={props.state.posts} addPost={props.addPost} />
+      <MyPosts posts={props.profilePage.posts}
+        newPostText={props.profilePage.newPostText}
+        dispatch={props.dispatch} />
     </div>
   );
 };
