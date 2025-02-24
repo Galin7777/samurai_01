ce4d7f81f2dc36a929cd4afe02548c27c8164a3f
Galin7777 <kazakova0309@yandex.ru>
2025-02-24 19:14:38 +0300

    Refactoring file

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -6,21 +6,8 @@ export const Message = (props) => {
   const messageClass = isLeft ? classes.messageLeft : classes.messageRight;
   const avatar = 'https://avatars.mds.yandex.net/i?id=2a0000017a084214d23eefb4c49e5ade2f46-4234071-images-thumbs&n=13';
 
-  const newMessageElement = React.createRef();
-
-  const addMessage = () => {
-    const text = newMessageElement.current.value;
-    alert(text);
-  }; 
-
   return (
     <div className={`${classes.message} ${messageClass}`}>
-      <div>
-        <textarea ref={newMessageElement}></textarea>
-      </div>
-      <div>
-        <button onClick={ addMessage }>Add message</button>
-      </div>
         <img src={avatar} alt='Avatar' className={classes.avatar} />
       <div className={classes.messageText}>{props.message}</div>
     </div>
