cad9a5e2cd3e9063b608a2b35bd027a3ae97460a
Galin7777 <kazakova0309@yandex.ru>
2025-03-09 11:47:13 +0300

    Delited rerenderEntireTree, added  Provider

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { store } from './redux/redux-store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const rerenderEntireTree = (state) => {
-  root.render(
+  
+root.render(
   <React.StrictMode>
-    <App state={state} dispatch={store.dispatch.bind(store)} />
+  <Provider store={store}>
+    <App />
+  </Provider >
   </React.StrictMode>
   );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  const state = store.getState();
-  rerenderEntireTree(state);
-});
