d8eaa7cc008d609652568e52e768f53f11810cd1
Galin7777 <kazakova0309@yandex.ru>
2025-03-31 12:44:33 +0300

    Refactor UsersContainer component: add user fetching from API, pagination management, and error handling.

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { Users } from './Users'
+import { Users } from './Users';
+import { setUsersAC } from '../../redux/users-reduser';
+import { setCurrentPageAC } from '../../redux/users-reduser'
+import { setTotalUsersCountAC } from '../../redux/users-reduser';
 
-export const UsersContainer = () => {
+export const UsersContainer = (props) => {
   const dispatch = useDispatch();
-  const { users } = useSelector(store => store.usersPage);
+  const { users, currentPage, totalUsersCount, pageSize } = useSelector(store => store.usersPage);
 
+  useEffect(() => {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${props.currentPage}&count=${props.pageSize}`)
+      .then(response => {
+        dispatch(setUsersAC(response.data.items));
+        dispatch(setTotalUsersCountAC(response.data.totalCount));
+      })
+      .catch(error => console.error('Ошибка при загрузке пользователей:', error));
+  }, [dispatch, props.currentPage, props.pageSize]);
+  
 const follow = (userId) => {
   dispatch({ type: 'FOLLOW', userId });
 };
@@ -15,14 +28,22 @@ const unfollow = (userId) => {
   dispatch({ type: 'UNFOLLOW', userId });
 };
 
-const setUsers = (users) => {
-  dispatch({ type: 'SET_USERS', users })
+const onPageChanged = (pageNumber) => {
+  dispatch(setCurrentPageAC(pageNumber));
+
+  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`)
+    .then(response => {
+      dispatch(setUsersAC(response.data.items));
+    });
 };
 
 return (<Users users={users}
+              currentPage={currentPage}
+              totalUsersCount={totalUsersCount}
+              pageSize={pageSize}
+              onPageChanged={onPageChanged}
               follow={follow}
               unfollow={unfollow}
-              setUsersAC={setUsers}
     />
   );
 };
