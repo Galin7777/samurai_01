44e203dde85ccce6d4b05f67c23a51f489f541a1
Galin7777 <kazakova0309@yandex.ru>
2025-04-13 09:40:50 +0300

    Added SET_USER_PROFILE

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 const initialState = {
   posts: [
@@ -7,6 +8,7 @@ const initialState = {
     { id: 2, message: 'It s my first post', likesCount: 25 },
   ],
   newPostText: 'it-kamasutra.com',
+  profile: null,
 };
 
 export const profileReducer = (state = initialState, action) => {
@@ -28,11 +30,19 @@ export const profileReducer = (state = initialState, action) => {
         ...state,
         newPostText: action.newText,
       };
+
+    case SET_USER_PROFILE:
+      return {
+        ...state,
+        profile: action.profile,
+      };
+
     default:
       return state;
   }
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 export const updateNewPostTextActionCreator = (text) =>
   ({ type: UPDATE_NEW_POST_TEXT, newText: text });
