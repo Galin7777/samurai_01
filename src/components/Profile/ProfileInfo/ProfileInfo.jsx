4a54ec933286fbaa89e919b7c728091bd72f3116
Galin7777 <kazakova0309@yandex.ru>
2025-04-13 09:39:45 +0300

    Add profile data rendering with conditional Preloader to ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,20 @@
 import  classes from './ProfileInfo.module.scss';
 import React from 'react';
+import { Preloader } from '../../common/Preloader/Preloader';
 
-export const ProfileInfo = () => {
+export const ProfileInfo = (props) => {
+  if (!props.profile) {
+    return <Preloader />;
+  }
+
   return (
     <div>
       <div>
         <img src='https://avatars.mds.yandex.net/i?id=c85e9599c8cbbff810260e0081cf57454b817d14-10981924-images-thumbs&n=13' alt='' />
       </div>
       <div className={classes.descriptionBlock}>
+        <img src={props.profile.photos.large}/>
+        {/* <div>contacts={props.profile.contacts}</div> */}
         ava + description
       </div>
     </div>
