eee06d1df858cc1d4160462bfc5a58e778e35ff2
Galin7777 <kazakova0309@yandex.ru>
2025-04-26 11:29:03 +0300

    Added users list refresh after Follow/Unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import classes from './Users.module.scss';
 import userPhoto from '../../../src/assets/images/avatar.jpg';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { followAC } from '../../redux/users-reduser';
+import { unfollowAC } from '../../redux/users-reduser';
 
-export const Users = ({ users, currentPage, pages, onPageChanged, follow, unfollow, portionSize }) => {
+export const Users = ({ users, currentPage, pages, onPageChanged, portionSize, fetchUsers }) => {
+  const dispatch = useDispatch();
+  const [loadingIds, setLoadingIds] = useState([]);
+
+  const API_URL = 'https://social-network.samuraijs.com/api/1.0/';
+  const API_KEY = '96903f6e-7c99-43f5-8eb7-569b44830df5';
+
   const portionNumber = Math.ceil(currentPage / portionSize);
   const startIndex = (portionNumber - 1) * portionSize;
   const paginatedPages = pages.slice(startIndex, startIndex + portionSize);
 
+  const handleFollow = async (userId) => {
+    setLoadingIds((prev) => [...prev, userId]);
+
+    try {
+      const response = await axios.post(`${API_URL}follow/${userId}`, {}, {
+        withCredentials: true,
+        headers: { 'API-KEY': API_KEY },
+      });
+
+      if (response.data.resultCode === 0) {
+        dispatch(followAC(userId));
+        await fetchUsers(); // обновляем список после Follow
+      }
+    } catch (error) {
+      console.error('Ошибка при подписке:', error);
+    } finally {
+      setLoadingIds((prev) => prev.filter((id) => id !== userId));
+    }
+  };
+
+  const handleUnfollow = async (userId) => {
+    setLoadingIds((prev) => [...prev, userId]);
+
+    try {
+      const response = await axios.delete(`${API_URL}follow/${userId}`, {
+        withCredentials: true,
+        headers: { 'API-KEY': API_KEY },
+      });
+
+      if (response.data.resultCode === 0) {
+        dispatch(unfollowAC(userId));
+        await fetchUsers(); // обновляем список после Unfollow
+      }
+    } catch (error) {
+      console.error('Ошибка при отписке:', error);
+    } finally {
+      setLoadingIds((prev) => prev.filter((id) => id !== userId));
+    }
+  };
+
   return (
     <div className={classes.usersContainer}>
-      {/* Пагинация с порциями и стрелками */}
+      {/* Пагинация */}
       <div>
         {startIndex > 0 && (
           <button onClick={() => onPageChanged(startIndex)}>{'<'}</button>
@@ -35,12 +85,13 @@ export const Users = ({ users, currentPage, pages, onPageChanged, follow, unfoll
       {users.map((user) => (
         <div key={user.id} className={classes.userCard}>
           <div className={classes.avatar}>
-            <NavLink to={'/profile/' + user.id}>
+            <NavLink to={`/profile/${user.id}`}>
               <img src={user.photos.small || userPhoto} alt='avatar' />
             </NavLink>
             <button
               className={user.followed ? classes.unfollowButton : classes.followButton}
-              onClick={() => (user.followed ? unfollow(user.id) : follow(user.id))}
+              onClick={() => user.followed ? handleUnfollow(user.id) : handleFollow(user.id)}
+              disabled={loadingIds.includes(user.id)}
             >
               {user.followed ? 'Unfollow' : 'Follow'}
             </button>
