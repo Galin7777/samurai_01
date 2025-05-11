a871010851795d835783fcf4dd07a09ed8c7ef7d
Galin7777 <kazakova0309@yandex.ru>
2025-05-11 11:24:38 +0300

    Moved user fetching logic into getUsers thunk

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,36 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Users } from './Users';
-import { setUsersAC } from '../../redux/users-reduser';
 import { setCurrentPageAC } from '../../redux/users-reduser';
-import { setTotalUsersCountAC } from '../../redux/users-reduser';
-import { toggleIsFetchingAC } from '../../redux/users-reduser';
 import { Preloader } from '../common/Preloader/Preloader';
-import { usersAPI } from '../../api/api';
+import { getUsers } from '../../redux/users-reduser';
 
 export const UsersContainer = () => {
   const dispatch = useDispatch();
   const { users, currentPage, totalUsersCount, pageSize, isFetching } = useSelector((store) => store.usersPage);
 
-  const fetchUsers = async (pageNumber = currentPage) => {
-    dispatch(toggleIsFetchingAC(true));
-    try {
-      const response = await usersAPI.getUsers(pageNumber, pageSize);
-      dispatch(setUsersAC(response.items));
-      dispatch(setTotalUsersCountAC(response.totalCount));
-    } catch (error) {
-      console.error('Ошибка при загрузке пользователей:', error);
-    }
-    dispatch(toggleIsFetchingAC(false));
-  };
-
   useEffect(() => {
-    fetchUsers();
+    dispatch(getUsers(currentPage, pageSize));
   }, [dispatch, currentPage, pageSize]);
 
   const onPageChanged = (pageNumber) => {
     dispatch(setCurrentPageAC(pageNumber));
-    fetchUsers(pageNumber);
   };
 
   const pagesCount = Math.ceil(totalUsersCount / pageSize);
@@ -46,7 +30,6 @@ export const UsersContainer = () => {
           pages={pages}
           onPageChanged={onPageChanged}
           portionSize={10}
-          fetchUsers={fetchUsers}
         />
       )}
     </>
