84044bf7c173575f922e20bb5bcd422baeeb303e
Galin7777 <kazakova0309@yandex.ru>
2025-05-11 11:29:33 +0300

    Moved authorization data fetching logic to Redux

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { Header } from './Header';
 import { useDispatch, useSelector } from 'react-redux';
-import { setAuthUserData } from '../../redux/auth-reduser';
-import { authAPI } from '../../api/api';
+import { getAuthUserData } from '../../redux/auth-reduser';
 
 export const HeaderContainer = () => {
   const dispatch = useDispatch();
@@ -10,20 +9,7 @@ export const HeaderContainer = () => {
   const { isAuth, login } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    const fetchAuthData = async () => {
-      try {
-        const response = await authAPI.getHeaders();
-
-        if (response.resultCode === 0) {
-          const { id, email, login } = response.data;
-          dispatch(setAuthUserData(id, email, login));
-        }
-      } catch (error) {
-        console.error('Ошибка при получении авторизационных данных:', error);
-      }
-    };
-
-    fetchAuthData();
+    dispatch(getAuthUserData());
   }, [dispatch]);
 
   return <Header isAuth={isAuth} login={login} />;
