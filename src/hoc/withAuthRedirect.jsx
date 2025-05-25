0b34b5d02ad65d9a680352055d387736659b035f
Galin7777 <kazakova0309@yandex.ru>
2025-05-25 12:37:14 +0300

    Added isLoading check in withAuthRedirect to prevent redirect to /login before profile loads

--- src/hoc/withAuthRedirect.jsx
+++ src/hoc/withAuthRedirect.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { toggleProfileLoading } from '../redux/profile-reducer';
 
 export const withAuthRedirect = (Component) => {
   return (props) => {
     const isAuth = useSelector((state) => state.auth.isAuth);
-    if (!isAuth) return <Navigate to={'/login'} />;
+    if (!isAuth && !toggleProfileLoading) return <Navigate to={'/login'} />;
     return <Component {...props} />;
   };
 };
