fcd04e012148c20d9d3a4362c604065e14fe8143
Galin7777 <kazakova0309@yandex.ru>
2025-05-29 08:17:56 +0300

    Added getStatus and updateStatus methods to profileAPI to get and update user status

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,24 +5,27 @@ import { Profile } from './Profile';
 import { getUserProfile } from '../../redux/profile-reducer';
 import { withAuthRedirect } from '../../../src/hoc/withAuthRedirect';
 import { Preloader } from '../common/Preloader/Preloader';
+import { getStatus } from '../../redux/profile-reducer';
+import { updateStatus } from '../../redux/profile-reducer';
 
 const ProfileContainer = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
   const profile = useSelector((state) => state.profilePage.profile);
   const isLoading = useSelector((state) => state.profilePage.isLoading);
-
+  const status = useSelector((state) => state.profilePage.status);
 
   useEffect(() => {
     if (userId) {
       dispatch(getUserProfile(userId));
+      dispatch(getStatus(userId));
     }
   }, [dispatch, userId]);
 
   if (isLoading || !profile) {
     return <Preloader />;
   }
-  return <Profile profile={profile}/>;
+  return < Profile profile={profile} status={status} updateStatus={(status) => dispatch(updateStatus(status))}/>;
 };
 
 export default withAuthRedirect(ProfileContainer);
