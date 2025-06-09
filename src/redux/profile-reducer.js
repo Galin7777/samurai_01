ecfd30a65d02d4728eae86062ad7f6614d19a876
Galin7777 <kazakova0309@yandex.ru>
2025-06-09 17:02:14 +0300

    Remove newPostText from state and drop UPDATE_NEW_POST_TEXT; new post text is now passed directly in the ADD_POST action payload.

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 import { profileAPI } from '../api/api';
 
 const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const TOGGLE_PROFILE_LOADING = 'TOGGLE_PROFILE_LOADING';
 const SET_STATUS = 'SET_STATUS';
@@ -12,7 +11,6 @@ const initialState = {
     { id: 1, message: 'Hi Andrey', likesCount: 5 },
     { id: 2, message: 'It s my first post', likesCount: 25 },
   ],
-  newPostText: 'it-kamasutra.com',
   profile: null,
   isLoading: false,
   status: '',
@@ -24,18 +22,12 @@ export const profileReducer = (state = initialState, action) => {
     case ADD_POST:
       const newPost = {
         id: state.posts.length + 1,
-        message: state.newPostText,
+        message: action.newPostText,
         likesCount: 0,
       };
       return {
         ...state,
         posts: [...state.posts, newPost],
-        newPostText: '',
-      };
-    case UPDATE_NEW_POST_TEXT:
-      return {
-        ...state,
-        newPostText: action.newText,
       };
 
     case SET_STATUS:
@@ -61,10 +53,8 @@ export const profileReducer = (state = initialState, action) => {
   }
 };
 
-export const addPostActionCreator = () => ({ type: ADD_POST });
+export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostText });
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
-export const updateNewPostTextActionCreator = (text) =>
-  ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 export const toggleProfileLoading = (isLoading) => ({ type: TOGGLE_PROFILE_LOADING, isLoading });
 export const setStatus = (status) => ({ type: SET_STATUS, status });
 
