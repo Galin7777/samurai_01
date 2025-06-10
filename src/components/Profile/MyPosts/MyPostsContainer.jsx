9ed1fb82b6100a7dc2eb0e82ca24fb9ad867be27
Galin7777 <kazakova0309@yandex.ru>
2025-06-10 11:19:47 +0300

    Removed new post text update logic from Redux

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,21 +3,20 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { MyPosts } from './MyPosts';
 import { addPostActionCreator } from '../../../redux/profile-reducer';
-import { updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 
 export const MyPostsContainer = () => {
   const dispatch = useDispatch();
   const { posts, newPostText } = useSelector((state) => state.profilePage);
 
-  const addPost = () => {
-    dispatch(addPostActionCreator());
+  const addPost = (newPostText) => {
+    dispatch(addPostActionCreator(newPostText));
   };
 
-  const onPostChange = (text) => {
-    dispatch(updateNewPostTextActionCreator(text));
-  };
+  // const onPostChange = (text) => {
+  //   dispatch(updateNewPostTextActionCreator(text));
+  // };
 
-  return (<MyPosts updateNewPostText={onPostChange} addPost={addPost}
+  return (<MyPosts addPost={addPost}
     posts={posts}
     newPostText={newPostText} />);
 };
