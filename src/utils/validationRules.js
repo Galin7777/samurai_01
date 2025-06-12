241014a4048c375b7faf1e6d46c62089500660ab
Galin7777 <kazakova0309@yandex.ru>
2025-06-12 10:27:14 +0300

    Fix

--- src/utils/validationRules.js
+++ src/utils/validationRules.js
@@ -1,6 +1,3 @@
 export const requiredForm = {
   required: 'Заполните поле',
-  minLength: 5,
-  // <p>{getFieldState('firstName').isTouched && 'touched'}</p>;
-  // < select name = { name } ref = { ref } onChange = { onChange } onBlur = { onBlur } />
 };
