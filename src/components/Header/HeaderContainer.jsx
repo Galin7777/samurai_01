28609679cbfc4c4bde6eef165f17273bd2de7766
Galin7777 <kazakova0309@yandex.ru>
2025-06-14 17:51:22 +0300

    Add logout handler and pass it to Header component

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Header } from './Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAuthUserData } from '../../redux/auth-reduser';
+import { logout } from '../../redux/auth-reduser';
+
 
 export const HeaderContainer = () => {
   const dispatch = useDispatch();
 
   const { isAuth, login } = useSelector((state) => state.auth);
 
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   useEffect(() => {
     dispatch(getAuthUserData());
   }, [dispatch]);
 
-  return <Header isAuth={isAuth} login={login} />;
+  return <Header isAuth={isAuth} login={login} logout={handleLogout} />;
 };
