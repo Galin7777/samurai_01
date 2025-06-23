43968d2b682b2c72d1e8d23f7cc21373a301b28c
Galin7777 <kazakova0309@yandex.ru>
2025-06-23 12:18:37 +0300

    Add app initialization with Preloader and refactor App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import classes from './App.module.scss';
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Navbar } from './components/Navbar';
 import { Navigate } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-import  DialogsContainer  from './components/Dialogs/DialogsContainer';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { UsersContainer } from './components/Users/UsersContainer';
-import  ProfileContainer  from './components/Profile/ProfileContainer';
+import { ProfileContainer } from './components/Profile/ProfileContainer';
 import { HeaderContainer } from './components/Header/HeaderContainer';
 import { LoginForm } from './components/Login/LoginForm';
+import { Preloader } from './components/common/Preloader/Preloader';
+import { initializeApp } from './redux/app-reduser';
 
 export const App = () => {
+  const dispatch = useDispatch();
   const sidebar = useSelector((state) => state.sidebar);
-  const isAuth = useSelector((state) => state.auth.isAuth);
+  const initialized = useSelector((state) => state.app.initialized);
+
+  useEffect(() => {
+    dispatch(initializeApp());
+  }, [dispatch]);
+
+
+  if (!initialized) {
+    return <Preloader />;
+  }
 
   return (
     <BrowserRouter>
@@ -24,7 +37,7 @@ export const App = () => {
         <div className={classes.appWrapperContent}>
           <Routes>
             <Route path='/profile' element={<Navigate to='/profile/32285' />} />
-            <Route path='/profile/:userId' element={ <ProfileContainer />} />
+            <Route path='/profile/:userId' element={<ProfileContainer />} />
             <Route path='/dialogs' element={<DialogsContainer />} />
             <Route path='/users' element={<UsersContainer />} />
             <Route path='/login' element={<LoginForm />} />
@@ -37,3 +50,4 @@ export const App = () => {
     </BrowserRouter>
   );
 };
+
