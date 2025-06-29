014cb0f468f19bf72c6ce803b660c056eb52a3c7
Galin7777 <kazakova0309@yandex.ru>
2025-06-29 11:46:19 +0300

    Refactor UsersContainer: replaced useSelector with selectors, moved some logic to reducer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,24 +1,33 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Users } from './Users';
 import { setCurrentPageAC } from '../../redux/users-reduser';
 import { Preloader } from '../common/Preloader/Preloader';
 import { getUsers } from '../../redux/users-reduser';
+import { selectUsers } from '../../redux/users-selectors';
+import { selectCurrentPage } from '../../redux/users-selectors';
+import { selectPageSize } from '../../redux/users-selectors';
+import { selectTotalUsersCount } from '../../redux/users-selectors';
+import { selectIsFetching } from '../../redux/users-selectors';
 
 export const UsersContainer = () => {
   const dispatch = useDispatch();
-  const { users, currentPage, totalUsersCount, pageSize, isFetching } = useSelector((store) => store.usersPage);
+  const users = useSelector(selectUsers);
+  const currentPage = useSelector(selectCurrentPage);
+  const pageSize = useSelector(selectPageSize);
+  const totalUsersCount = useSelector(selectTotalUsersCount);
+  const isFetching = useSelector(selectIsFetching);
 
   useEffect(() => {
-    dispatch(getUsers(currentPage, pageSize));
-  }, [dispatch, currentPage, pageSize]);
+    dispatch(getUsers(currentPage, pageSize)); // вынесла в редьюсер
+  }, [dispatch, currentPage, pageSize]); // вынесла
 
   const onPageChanged = (pageNumber) => {
     dispatch(setCurrentPageAC(pageNumber));
   };
 
-  const pagesCount = Math.ceil(totalUsersCount / pageSize);
+  const pagesCount = Math.ceil(totalUsersCount / pageSize); // вынесла в редьюсер
   const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
 
   return (
