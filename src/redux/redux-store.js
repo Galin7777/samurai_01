ad82402425d177ab3406247f86263909f2bd8f18
Galin7777 <kazakova0309@yandex.ru>
2025-08-16 11:23:18 +0300

    store: removed explicit thunk, using default middleware

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,7 +5,6 @@ import { dialogsReducer } from './dialogs-reducer';
 import { sidebarReducer } from './sidebar-reducer';
 import { usersReducer } from './users-reduser';
 import { authReducer } from './auth-reduser';
-import { thunk } from 'redux-thunk';
 import { appReducer } from './app-reduser';
 
 export const reducer = combineReducers({
@@ -19,5 +18,4 @@ export const reducer = combineReducers({
 
 export const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
