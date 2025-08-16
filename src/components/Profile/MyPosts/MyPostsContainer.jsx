20fa6a64cd8dc6890c19e978e8348e2de16006e9
Galin7777 <kazakova0309@yandex.ru>
2025-08-16 11:12:53 +0300

    Refactor MyPostsContainer: removed unused newPostText props

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,19 +1,20 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-// import { shallowEqual } from 'react-redux';
 import { MyPosts } from './MyPosts';
 import { addPostActionCreator } from '../../../redux/profile-reducer';
 
 export const MyPostsContainer = () => {
   const dispatch = useDispatch();
-  const { posts, newPostText } = useSelector((state) => state.profilePage);
+  const { posts } = useSelector((state) => state.profilePage);
 
   const addPost = useCallback((newPostText) => {
     dispatch(addPostActionCreator(newPostText));
   }, [dispatch]);
 
-  return (<MyPosts addPost={addPost}
+  return (<MyPosts
+    addPost={addPost}
     posts={posts}
-    newPostText={newPostText} />);
+  />
+  );
 };
