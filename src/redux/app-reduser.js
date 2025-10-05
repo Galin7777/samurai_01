cfd69b3965a51c771e539702b8ba675748ce48c2
Galin7777 <kazakova0309@yandex.ru>
2025-10-05 10:51:55 +0300

    Rewrote app initialization using createSlice and createAsyncThunk for cleaner Redux logic

--- src/redux/app-reduser.js
+++ src/redux/app-reduser.js
@@ -1,33 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getAuthUserData } from './auth-reduser';
-
-const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS';
 
 const initialState = {
   initialized: false,
 };
 
-export const appReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INITIALIZED_SUCCESS:
-      return {
-        ...state,
-        initialized: true,
-      };
+export const initializeApp = createAsyncThunk(
+  'app/initializeApp',
+  async (_, { dispatch }) => {
+    await dispatch(getAuthUserData());
+    return true;
+  },
+);
 
-    default:
-      return state;
-
-  };
-};
-
-export const initializedSuccess = () => ({ type: INITIALIZED_SUCCESS });
-
-export const initializeApp = () => async (dispatch) => {
-  try {
-    await dispatch(getAuthUserData()); // просто ждём завершения
-  } catch (error) {
-    console.error('Ошибка при получении авторизационных данных:', error);
-  } finally {
-    dispatch(initializedSuccess()); // вызывается всегда, даже при ошибке
-  }
-};
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(initializeApp.fulfilled, (state) => {
+      state.initialized = true;
+    });
+  },
+});
